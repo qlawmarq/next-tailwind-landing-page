46552902e3a2f0f31f0a3b1b3553a6e8d6d9932b
Masaki <yoshiiwamasaki@gmail.com>
2022-04-04 20:13:40 +0900

    update CTA template

--- components/templates/CTA/index.tsx
+++ components/templates/CTA/index.tsx
@@ -26,20 +26,24 @@ export const CTA: React.FC<Props> = ({
           <div className="relative sm:w-6/12">
             <Image className="" src={image} alt="" />
           </div>
-          <div className="relative ml-auto mt-8 mb-16 flex flex-col items-start sm:mt-0 sm:mb-0 sm:w-5/12 sm:items-end sm:text-right xl:w-4/12 xl:pt-20">
+          <div className="relative ml-auto mt-8 mb-16 flex flex-col items-start text-left sm:mt-0 sm:mb-0 sm:w-6/12 xl:w-8/12 xl:pt-20">
             <H1 className="mb-4">{title}</H1>
-            <H5 className="mb-4 sm:mb-12">{description}</H5>
-            <Button onClick={onClickStart}>Start</Button>
+            <H5 className="mb-2">{description}</H5>
+            <Button className="mt-4" onClick={onClickStart}>
+              Start
+            </Button>
           </div>
         </div>
       )}
 
       {isImageRight && (
         <div className="relative mx-auto flex w-full flex-col-reverse  sm:flex-row">
-          <div className="relative mr-auto mt-8 mb-16 flex flex-col items-start sm:mt-0 sm:mb-0 sm:w-5/12 sm:items-start sm:text-left xl:w-4/12 xl:pt-20">
+          <div className="relative mr-auto mt-8 mb-16 flex flex-col items-start text-left sm:mt-0 sm:mb-0 sm:w-6/12 xl:w-8/12 xl:pt-20">
             <H1 className="mb-4">{title}</H1>
-            <H5 className="mb-4 sm:mb-12">{description}</H5>
-            <Button onClick={onClickStart}>Start</Button>
+            <H5 className="mb-2">{description}</H5>
+            <Button className="mt-4" onClick={onClickStart}>
+              Start
+            </Button>
           </div>
           <div className="relative sm:w-6/12">
             <Image className="" src={image} alt="" />
